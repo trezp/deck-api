92ebeabc9367da7c1a6a3566b850d558e37f6a0c
Treasure <treasure.rose@gmail.com>
2018-03-08 23:22:24 -0800

    Deck with cards in it, woo

--- models/cards.js
+++ models/cards.js
@@ -1,18 +1,51 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true)
 
+const ValueCardSchema = mongoose.Schema({
+  type: {type: String, default: "mileage"},
+  played: {type: Boolean, default: false},
+  value: Number
+});
 
-const CardSchema = mongoose.Schema({
+const SpecialCardSchema = mongoose.Schema({
   name: String,
-  value: Number,
-  special: String
+  played: {type: Boolean, default: false},
+  special: {type: Boolean, default: true}
 });
 
-const DeckSchema= mongoose.Schema({
-  deck: [CardSchema]
+const DeckSchema = mongoose.Schema({
+  specialCards: [SpecialCardSchema],
+  mileageCards: [ValueCardSchema]
 });
 
-const Card = mongoose.model('Card', CardSchema);
-const Deck = mongoose.model('Deck', DeckSchema)
+const ValueCard = mongoose.model('ValueCard', ValueCardSchema);
+const SpecialCard = mongoose.model('SpecialCard', SpecialCardSchema);
+const Deck = mongoose.model('Deck', DeckSchema);
 
-module.exports = {Card, Deck}
+
+function makeValueCard(value, quantity){
+  for(let i = 1; i <= quantity; i++){
+    let card = new ValueCard({
+      value: value
+    })
+    deck.mileageCards.push(card);
+  }
+}
+
+function makeSpecialCard(name, quantity){
+  for(let i = 1; i <= quantity; i++){
+    let card = new SpecialCard({
+      name: name,
+    })
+    deck.specialCards.push(card);
+  }
+}
+
+const deck = new Deck;
+makeSpecialCard("No Rocket Fuel", 3);
+makeSpecialCard("Black Hole", 3);
+makeValueCard(50, 5);
+makeValueCard(100, 5);
+
+
+module.exports = deck
